670ea0ab18f2ae1e6295ba3f7bcd841649a0f65c
zhuzhenyuan <zhenyuanzhu@outlook.com>
2020-06-03 18:46:31 +0800

    fix bug

--- durian_client.py
+++ durian_client.py
@@ -1,13 +1,12 @@
+import json
 import os
 import sys
 
-base_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-sys.path.append(os.path.join(base_path, "./"))
-
-import json
 from lib.cipher import get_cipher
 from lib.config import client_config__handler
 
+base_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+sys.path.append(os.path.join(base_path, "./"))
 import tornado
 
 from service.client import Socks5Client, TunnelClient
@@ -16,14 +15,15 @@ cfgs = {}
 
 
 def read_config():
+    global cfgs
     with open(os.path.join(base_path, "config.json"), "r") as f:
         content = f.read()
         cfgs = json.loads(content)
     cfgs = client_config__handler(cfgs)
-    return cfgs
 
 
 async def run():
+    global cfgs
     cfg = cfgs.get('Socks5Client', None)
     if cfg:
         socks5_client = Socks5Client()
@@ -39,9 +39,21 @@ async def run():
         await tunnel_client.start()
 
 
+def run2():
+    global cfgs
+    cfg = cfgs.get('Socks5Client', None)
+    if cfg:
+        socks5_client = Socks5Client()
+        socks5_client.set_remote_address(cfg['remote_host'], cfg['remote_port'])
+        if cfg.get('key'):
+            socks5_client.set_cipher(get_cipher('caesar', cfg['key']))
+        socks5_client.listen(cfg['local_port'], cfg['local_host'] if cfg.get('local_host') else '')
+
+    tornado.ioloop.IOLoop.current().start()
 if __name__ == '__main__':
     read_config()
     if cfgs.get('TunnelClient', None):
         tornado.ioloop.IOLoop.current().run_sync(run)
     else:
-        tornado.ioloop.IOLoop.current().start()
+        run2()
+
